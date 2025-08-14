d26bb98331773c45daee471094a68bf8c7fdfa3c
jigisha <vermajigisha03@gmail.com>
2025-08-14 23:19:53 +0530

    Made few changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import serverless from 'serverless-http';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors({
     origin: true,
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(expressApp);
+
+
